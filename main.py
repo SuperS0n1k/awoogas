b42075d18ad4ae1848d6a08fa135aa31ac5d85de
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-24 17:34:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ session = scratch3.login("TheBurgerFollower", "TheBurger")
 while True:
     project = session.connect_project("884084370")
     project.post_comment(content="#MakeScratchBurgerAgain")'''
+'''session = scratch3.login("BurgerArmy1", "TheBurger")
+    user = session.connect_user("griffpatch")
+    time.sleep(1)
+    user.post_comment("Become A Follower")
+    time.sleep(1)
+'''
 
 import time
 import scratchattach as scratch3
@@ -11,18 +17,6 @@ while True:
     session = scratch3.login("TheBurgerFollower", "TheBurger")
     user = session.connect_user("griffpatch")
     time.sleep(1)
-    user.follow()
-    time.sleep(1)
     user.post_comment("Become A Follower")
     time.sleep(1)
-    user.unfollow()
-    time.sleep(1)
-    session = scratch3.login("BurgerArmy1", "TheBurger")
-    user = session.connect_user("griffpatch")
-    time.sleep(1)
-    user.follow()
-    time.sleep(1)
-    user.post_comment("Become A Follower")
-    time.sleep(1)
-    user.unfollow()
-    time.sleep(1)
+    
