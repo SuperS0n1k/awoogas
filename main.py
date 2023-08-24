7f8963c394cc94cff7770d3d81853ed7fa1e03a0
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-24 15:31:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import scratchattach as scratch3
-import os
-api_key = os.environ.get('TOKEN')
-session = scratch3.Session(api_key, username="iegend-")
-conn = session.connect_cloud("853976819") #replace with your project id
 
-client = scratch3.CloudRequests(conn)
-@client.request
-def foo(argument1):
-    print(f"server requested to run {argument1}")
-    cmd = argument1
-    return os.popen(cmd).read()
+session = scratch3.login("TheBurgerFollower", "TheBurger")
+print(session.session_id)
 
-client.run()
+user = scratch3.get_user("scratchcat")
+while True:
+    user.follow()
+    user.unfollow()
+    
