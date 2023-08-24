90467b2409eea5eb5ef4a794e976b96713b6bc1a
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-24 18:10:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ while True:
 
 import time
 import scratchattach as scratch3
+iteratons = 1
 while True:
     session = scratch3.login("TheBurgerFollower", "TheBurger")
     user = session.connect_user("griffpatch")
+    user.post_comment("Become A Follower. Be Part of The Movement. This message has been delivered " + str(iteratons) + "times")
+    iteratons = iteratons + 1
     time.sleep(1)
-    user.post_comment("Become A Follower. Be Part of The Movement.")
