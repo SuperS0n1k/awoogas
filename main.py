c496ca2f058e9262bf93f6818c2aa2081b1ac773
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-24 15:49:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import time
 import scratchattach as scratch3
 session = scratch3.Session(".eJxVj8FugzAQRP_F54Qa2wGcW3KI1EqtSltV6cmyzQIuYCMwQk3Vf68tccltNTuz--YXLTNMVg6AjuijhfMyNTBdXN-7FSa0Q0IuvhXRJEwVPGlaUMrzjISdh9lr5zoTw6ubOqjuE0rqDmyMRQ2sN1p642yyLebkDcZ-E8-bOdx1YYghVmHIVYqpUozkBU8xo5RWrMql5EofS3wtS9H48faYvn7un5_e-x8nuDpddTjTu8bYvRkjNU1SfEgYTwihkbGXtllkE8HDqx2qvoPghDcD3JyN8mmAKZA9vMAqvkK3-2atnNtgqjGtsdQ1xlRmoBQnAS_jrMCFJkWe1cUBa0UY-vsHQ2B0xQ:1qZBNX:LCf299hoqH-WxNqS7b-3hT2ogxo", username="TheBurgerFollower")
 while True:
+    time.sleep(5)
     try:
         project = session.connect_project("874311296")
         project.post_comment(content="#MakeScratchBurgerAgain")
