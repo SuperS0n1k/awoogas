4817708d3913a6e44977bf8776fb5b0efc5be672
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-24 16:11:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-import time
+'''import time
 import scratchattach as scratch3
 session = scratch3.login("TheBurgerFollower", "TheBurger")
 while True:
     project = session.connect_project("884084370")
-    project.post_comment(content="#MakeScratchBurgerAgain")
+    project.post_comment(content="#MakeScratchBurgerAgain")'''
+
+import time
+import scratchattach as scratch3
+
+session = scratch3.ScratchSession()
+session.login("TheBurgerFollower", "TheBurger")
+
+while True:
+    try:
+        project = session.connect_project("884084370")
+        project.post_comment("#MakeScratchBurgerAgain")
+    except scratch3.ScratchConnectionError:
+        print("Failed to connect to Scratch project. Retrying in 5 seconds...")
+        time.sleep(5)
