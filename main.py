ba2846c4e37e32d1e21ee2d81a9c79071b311824
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-24 17:29:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,17 @@ while True:
 
 import time
 import scratchattach as scratch3
-session = scratch3.login("TheBurgerFollower", "TheBurger")
 while True:
+    session = scratch3.login("TheBurgerFollower", "TheBurger")
+    user = session.connect_user("griffpatch")
+    time.sleep(1)
+    user.follow()
+    time.sleep(1)
+    user.post_comment("Become A Follower")
+    time.sleep(1)
+    user.unfollow()
+    time.sleep(1)
+    session = scratch3.login("BurgerArmy1", "TheBurger")
     user = session.connect_user("griffpatch")
     time.sleep(1)
     user.follow()
