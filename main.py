88bd0b66c804f431958c0721dc399f8f9be69cf0
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-24 17:00:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ import time
 import scratchattach as scratch3
 session = scratch3.login("TheBurgerFollower", "TheBurger")
 while True:
-    project = session.connect_project("884084370")
-    project.favorite()
+    user = session.connect_user("griffpatch")
     time.sleep(1)
-    project.unfavorite()
+    user.follow()
+    time.sleep(1)
+    user.post_comment("Become A Follower")
+    time.sleep(1)
+    user.unfollow()
     time.sleep(1)
