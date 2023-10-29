91742c9f409f65f47b14e785a189ca2de40b784b
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-10-29 16:43:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,13 @@ from random import randint
 iteratons = 1
 while True:
     session = scratch3.login("qsefthukl", "qsefthuk")
-    user = session.connect_user("griffpatch")
+    '''user = session.connect_user("griffpatch")
     user.post_comment(str(randint(1,999999999999999999999999)))
     iteratons = iteratons + 1
-    time.sleep(1)
+    time.sleep(1)'''
+    project = session.connect_project("911931288")
+    project.love()
+    project.unlove()
+    project.favorite()
+    project.unfavorite()
+    project.post_view()
