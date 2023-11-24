2ac4d4f342a3dc4ff87625c065b5db1e39abfcfd
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-11-24 17:25:35 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-'''import time
+import os
+import sys
+import os.path
 import scratchattach as scratch3
-session = scratch3.login("TheBurgerFollower", "TheBurger")
-while True:
-    project = session.connect_project("884084370")
-    project.post_comment(content="#MakeScratchBurgerAgain")'''
-'''session = scratch3.login("BurgerArmy1", "TheBurger")
-    user = session.connect_user("griffpatch")
-    time.sleep(1)
-    user.post_comment("Become A Follower")
-    time.sleep(1)
-'''
+from scratchattach import Encoding
+from time import sleep
+username = 'iegend-' #add your username The username field is case sensitive
+sessionId = "sessionID" #add your sessionID
+session = scratch3.Session(sessionId, username=username)
+conn = session.connect_cloud('Your Remix project ID') #add your remixed project ID
+variables = scratch3.get_cloud('Your Remix project ID') #add your remixed project ID 
+conn = session.connect_cloud('843162693') #add Original Project ID
 
-import time
-import scratchattach as scratch3
-from random import randint
-iteratons = 1
-while True:
-    session = scratch3.login("qsefthukl", "qsefthuk")
-    '''user = session.connect_user("griffpatch")
-    user.post_comment(str(randint(1,999999999999999999999999)))
-    iteratons = iteratons + 1
-    time.sleep(1)'''
-    project = session.connect_project("911931288")
-    project.post_view()
+def run_forever():
+        try:
+                while True:
+                        for key in variables:
+                                conn = scratch3.TwCloudConnection(project_id = "Your Remix project ID", username=username) #add your remixed project ID 
+                                var = conn.get_var(key)
+                                conn = session.connect_cloud('843162693') #add Original Project ID
+                                if var == 'None':
+                                        #do nothing
+                                        print(var)
+                                else:
+                                        conn.set_var(key, var)
+                                        print(var)
+        except Exception:
+                print("error")
+                handle_exception()
+                run_forever()
+	
+def handle_exception():
+        # code here
+        pass
+    
+run_forever()
