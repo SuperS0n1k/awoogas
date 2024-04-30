8824d252bc21d8032b94b63daac93e9a1d07cd8a
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2024-04-30 15:38:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import time
+import random
 import scratchattach as scratch3
-import os
-api_key = os.environ.get('TOKEN')
-session = scratch3.Session(api_key, username="iegend-")
-conn = session.connect_cloud("853976819") #replace with your project id
 
-client = scratch3.CloudRequests(conn)
-@client.request
-def foo(argument1):
-    print(f"server requested to run {argument1}")
-    cmd = argument1
-    return os.popen(cmd).read()
-
-client.run()
+while True:
+	conn = scratch3.connect_tw_cloud("944067867")
+	q = scratch3.get_tw_var("523967150", "1")
+	conn.set_var("1", q)
+	conn.set_var("2", q)
+	conn.set_var("3", q)
+	conn.set_var("4", q)
+	conn.set_var("5", q)
+	conn.set_var("6", q)
+	conn.set_var("7", q)
+	conn.set_var("8", q)
+	conn.set_var("9", q)
